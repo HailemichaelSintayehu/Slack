1be33519998d03818a569a6f55755518cd3aa053
HailemichaelSintayehu <hmichaelsintayehu@gmail.com>
2022-07-24 13:38:09 +0300

    styled components added

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPp9J0vl536Rr4TYVw68ryEVu9WjcVZno",
+    authDomain: "slack-clone-8855f.firebaseapp.com",
+    projectId: "slack-clone-8855f",
+    storageBucket: "slack-clone-8855f.appspot.com",
+    messagingSenderId: "983610529483",
+    appId: "1:983610529483:web:8d7ca6c1faf8e14ee0ad4e"
+  };
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components" 
+function Header() {
+  return (
+    <div>
+ 
+    </div>
+  )
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+
+
+`
